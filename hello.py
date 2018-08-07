8365e5b011c41e009e0a53195e8ee13079b53b3f
Annaalek7a <42132667+Annaalek7a@users.noreply.github.com>
2018-08-07 14:16:36 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def appli(environ, start_response)#первая переменная - словарь, вторая - функция
-status = '200 OK'# код ответа
-headers = [('Content-Typy','text/plain')]
-start_response(status,headers)
-resp=environ['QUERY_STRING'].split("&")
-resp=[item+"\r+\n" for item in resp]
-return resp
+    status = '200 OK'# код ответа
+    headers = [('Content-Typy','text/plain')]
+    start_response(status,headers)
+    resp=environ['QUERY_STRING'].split("&")
+    resp=[item+"\r+\n" for item in resp]
+    return resp
