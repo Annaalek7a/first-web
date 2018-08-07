3d13c89eb853c2307c8ef0bd366a47e86d045de5
Annaalek7a <42132667+Annaalek7a@users.noreply.github.com>
2018-08-08 00:06:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8080 hello
