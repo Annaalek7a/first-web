2c3bc40bf82216d4e6ab05021b3c3b5e8f58b1db
Annaalek7a <42132667+Annaalek7a@users.noreply.github.com>
2018-08-07 23:16:28 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
-def appli(environ, start_response)#первая переменная - словарь, вторая - функция
- 
-    status = '200 OK'# код ответа
-    headers = [('Content-Typy','text/plain')]
-    start_response(status,headers)
-    resp = environ['QUERY_STRING'].split("&")
-    resp = [item+"\r+\n" for item in resp]
-    return resp
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = environ['QUERY_STRING']
+    body = '\n'.join(body.split('&')).encode('utf-8')
+    start_response(status, headers)
+    return iter([body])
